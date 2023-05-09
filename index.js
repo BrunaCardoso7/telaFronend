4b581e37b0610c321bdf7b8facc4251d15ad9fc7
Bruna Fernandes Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-05-09 10:14:43 -0300

    inicialização do projeto

---
+++ index.js
@@ -0,0 +1,15 @@
+class Produto{
+    constructor(){
+        this.id = 0;
+        this.nomeProduto = '';
+        this. valor = 0;
+    }
+    adicionar(){
+        alert('adiconado')
+    }
+    remover(){
+        alert('removido')
+    }
+
+}
+var produto = new Produto()
