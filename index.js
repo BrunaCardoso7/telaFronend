b3dfeec18360fb4400f29678007ee8e42386c894
Bruna Fernandes Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-05-09 13:51:22 -0300

    atulização

--- index.js
+++ index.js
@@ -1,15 +1,75 @@
 class Produto{
     constructor(){
-        this.id = 0;
-        this.nomeProduto = '';
-        this. valor = 0;
+        this.id = 1;
+        this.arrayprodutos = [];
+
+    }   
+    salvar(){
+        let produto = this.lerDados();
+
+        if(this.validaCampo(produto)){
+            this.adicionar(produto)
+        }
+
+        this.listaTabela(produto)
     }
-    adicionar(){
-        alert('adiconado')
+    lerDados(){
+        let produto = {};
+
+        produto.id = this.id
+        produto.nomeproduto = document.getElementById("nomeProduto").value;
+        produto.valorproduto = document.getElementById("valorProduto").value;
+
+        return produto
     }
-    remover(){
+    validaCampo(produto){
+        let msg = '';
+
+        if (produto.nomeproduto == ''){
+            msg += 'Preencha o campo produto\n';
+        }
+        if(produto.valorproduto == ''){
+            msg+= 'Preencha o campo valor';
+        }
+        if(msg != ''){
+            alert(msg);
+            return false
+        }
+        return true;
+    }
+    adicionar(produto){
+        this.arrayprodutos.push(produto)
+        this.id++
+    }
+    listaTabela(){
+        let tbody = document.getElementById('tbody');
+        tbody.innerText = ''
+        for(let i = 0; i < this.arrayprodutos.length; i++){
+            
+            let tr = tbody.insertRow()
+            let td_id = tr.insertCell()
+            let td_produto = tr.insertCell()
+            let td_valor = tr.insertCell()
+            let td_acao = tr.insertCell()
+
+            td_id.innerText = this.arrayprodutos[i].id
+            td_produto.innerText = this.arrayprodutos[i].nomeproduto
+            td_valor.innerText = this.arrayprodutos[i].valorproduto
+
+            td_id.classList.add('center')
+
+            let acaoEdit = document.createElement('img')
+            acaoEdit.src = 'edit.png'
+            
+            acaoEdit.classList.add('img_size')
+            
+            td_acao.appendChild(acaoEdit)
+
+        }
+    }
+    cancelar(){
         alert('removido')
     }
 
 }
-var produto = new Produto()
+var produto = new Produto();
